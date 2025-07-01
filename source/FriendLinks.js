b0f39e1ded7ebb576b0cde6c1099ae938c42def8
曦暮流年 <2515550574@qq.com>
2025-07-01 21:55:40 +0800

    初始化

---
+++ source/FriendLinks.js
@@ -0,0 +1,35 @@
+// 友链配置
+export const friendLinks = {
+    // 友链申请
+    apply: {
+        // 简介
+        description: [
+            "p:在网上冲浪时，总能遇到一些写得超棒的博客，里面的文章质量高得让人佩服",
+            "p:为了不和这么优秀的文章作者失之交臂，得赶紧和他们交换友情链接！这样不仅能分享好内容，还能结交志同道合的朋友，一起交流心得、共同成长",
+            "img:/img/friendLinks1.png",
+            "img:/img/friendLinks2.gif"
+        ],
+        // 本站申请信息
+        scheme: {
+            name: "曦暮流年",   // 名称
+            url: "https://www.ximuliunian.top",  // 链接
+            avatar: "https://www.ximuliunian.top/img/avatar.png",  // 头像
+            desc: "懒惰往往是创新的催化剂",  // 简介
+            bgColor: "#8bfdd4"  // 背景颜色
+        },
+        // 申请要求
+        require: [
+            "ol:贵站已经添加的本站的链接",
+            "ol:贵站在中国大陆能够正常访问",
+            "ol:贵站符合中国大陆法律",
+            "p:如果符合要求的话可以根据上面表格的格式进行申请友链，比如：",
+            "p:名称：xxx",
+            "p:简介：xxxxxxxxxx",
+            "p:填写完成之后在下方进行留言，至于本站信息就在上面表格之中"
+        ],
+    },
+    // 友链分类列表
+    links: [
+
+    ]
+}
